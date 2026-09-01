316b5ed265dcf0f9d4d1217a5e97ba617b820cc9
Anil1269 <kumarreddy.120@gmail.com>
2026-09-01 11:01:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+```javascript
+function openInvitation() {
+
+    const invitation =
+        document.getElementById("invitation");
+
+    invitation.scrollIntoView({
+        behavior: "smooth"
+    });
+
+}
+```
